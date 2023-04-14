54253303f334c86571830ebd8d8d24e0a1bc094f
Jiayao Yan <jyan@Jiayaos-MacBook-Pro.local>
2023-04-14 15:22:33 -0700

    remove python 3.8 tests

--- setup.py
+++ setup.py
@@ -39,9 +39,10 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        # 'Programming Language :: Python :: 3.7',
+        # 'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
