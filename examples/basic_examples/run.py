44e6cecc956b046cc0756da9704a555d3c531bad
Jiayao Yan <45738410+jiy352@users.noreply.github.com>
2023-06-06 16:59:12 -0700

    Update run.py

--- examples/basic_examples/run.py
+++ examples/basic_examples/run.py
@@ -5,5 +5,8 @@ import numpy as np
 
 file_name = '/vnv_meshes/byu_vortex_lattice/x.txt'
 full_path = os.path.abspath(file_name)
-x = np.loadtxt(full_path)
-print('the shape of x is: ', x.shape)
+print('full_path')
+print(full_path)
+
+# x = np.loadtxt(full_path)
+# print('the shape of x is: ', x.shape)
