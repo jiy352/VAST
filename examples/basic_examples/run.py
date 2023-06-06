d1a79339ad91bb466aa585e61e3b065adbfff5d7
Jiayao Yan <45738410+jiy352@users.noreply.github.com>
2023-06-06 16:36:53 -0700

    Update run.py

--- examples/basic_examples/run.py
+++ examples/basic_examples/run.py
@@ -3,7 +3,7 @@ from ozone.api import ODEProblem
 import os
 import numpy as np
 
-file_name = './examples/basic_examples/vnv_meshes/byu_vortex_lattice/x.txt'
+file_name = '../examples/basic_examples/vnv_meshes/byu_vortex_lattice/x.txt'
 full_path = os.path.abspath(file_name)
 x = np.loadtxt(full_path)
 print('the shape of x is: ', x.shape)
