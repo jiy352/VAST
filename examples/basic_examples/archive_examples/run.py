5c919b968f18b1384e97e5296837887dea4e707d
jiy352 <jiy352@ucsd.edu>
2023-09-14 16:23:06 -0700

    rough clean before tagging

--- examples/basic_examples/archive_examples/run.py
+++
@@ -1,9 +0,0 @@
-from ozone.api import ODEProblem
-
-import os
-import numpy as np
-
-file_name = '../examples/basic_examples/vnv_meshes/byu_vortex_lattice/x.txt'
-full_path = os.path.abspath(file_name)
-x = np.loadtxt(full_path)
-print('the shape of x is: ', x.shape)
