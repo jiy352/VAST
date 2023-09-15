29b5398788bf39274cf18ef9a654b7655582f071
jiy352 <jiy352@ucsd.edu>
2023-09-15 00:38:57 -0700

    update need to clean up compressible case next

---
+++ examples/basic_examples/unsteady_prescribed_wake/ex_3heaving_verification.py
@@ -0,0 +1,100 @@
+'''Example 3 : verification of prescibed vlm with Katz and Plotkin 1991'''
+from VAST.core.vlm_llt.vlm_dynamic_old.VLM_prescribed_wake_solver import UVLMSolver
+
+from VAST.utils.generate_mesh import *
+from VAST.utils.make_video_vedo import make_video as make_video_vedo
+import time
+import numpy as np
+# Script to create optimization problem
+
+be = 'python_csdl_backend'
+make_video = 0
+plot_cl = 1
+
+# This is a test case to check the prescribed wake solver
+########################################
+# 1. define geometry
+########################################
+nx = 5; ny = 7
+chord = 1; span = 4
+num_nodes = 42;  nt = num_nodes
+
+mesh_dict = {"num_y": ny, "num_x": nx, "wing_type": "rect",  "symmetry": False,
+                 "span": span, "root_chord": chord,"span_cos_spacing": False, "chord_cos_spacing": False}
+mesh = generate_mesh(mesh_dict)
+# this is the same geometry as the dynamic_simple.ji
+
+########################################
+# 2. define kinematics
+########################################
+n_period = 3 
+omg=1 
+h=0.1 * chord
+alpha = - np.deg2rad(0) 
+t_vec = np.linspace(0, n_period*np.pi*2, num_nodes) 
+
+u_val = (np.ones(num_nodes) * np.cos(alpha)).reshape((num_nodes,1)) 
+w_vel = np.ones((num_nodes,1)) * np.sin(alpha) - h * np.cos(omg*t_vec).reshape((num_nodes,1))
+# In dynamic_simple.ji there are only the first five elements of the vector, last one is missing
+# signs are the same
+# TODO: check wake geometry and wake velocity
+
+alpha_equ = np.arctan2(w_vel, u_val)
+
+states_dict = {
+    'u': u_val, 'v': np.zeros((num_nodes, 1)), 'w': w_vel,
+    'p': np.zeros((num_nodes, 1)), 'q': np.zeros((num_nodes, 1)), 'r': np.zeros((num_nodes, 1)),
+    'theta': alpha_equ, 'psi': np.zeros((num_nodes, 1)),
+    'x': np.zeros((num_nodes, 1)), 'y': np.zeros((num_nodes, 1)), 'z': np.zeros((num_nodes, 1)),
+    'phiw': np.zeros((num_nodes, 1)), 'gamma': np.zeros((num_nodes, 1)),'psiw': np.zeros((num_nodes, 1)),
+}
+
+surface_properties_dict = {'wing':(nx,ny,3)}
+
+# mesh_val = generate_simple_mesh(nx, ny, num_nodes)
+mesh_val = np.zeros((num_nodes, nx, ny, 3))
+z_offset = omg*h*sin(omg*t_vec) #* 0
+# z_offset = omg*h*sin(omg*t_vec) 
+
+for i in range(num_nodes):
+    mesh_val[i, :, :, :] = mesh
+    mesh_val[i, :, :, 0] = mesh.copy()[:, :, 0] 
+    mesh_val[i, :, :, 1] = mesh.copy()[:, :, 1] 
+    mesh_val[i, :, :, 2] += z_offset[i]
+
+h_stepsize = delta_t = t_vec[1] 
+
+import python_csdl_backend
+sim = python_csdl_backend.Simulator(UVLMSolver(num_times=nt,h_stepsize=h_stepsize,states_dict=states_dict,
+                                    surface_properties_dict=surface_properties_dict,mesh_val=mesh_val), mode='rev')
+    
+t_start = time.time()
+sim.run()
+
+print('simulation time is', time.time() - t_start)
+# print('theta',sim['theta'])
+######################################################
+# make video
+######################################################
+import matplotlib as mpl
+mpl.rcParams.update(mpl.rcParamsDefault)
+
+import matplotlib.pyplot as plt
+
+######################################################
+# end make video
+######################################################
+
+# plot geometry
+
+# plot the velocity of the surface
+surface_vel_z = -sim['wing_kinematic_vel'][:,0,2]
+plt.plot(sim['wing'][:,0,0,2],'.-')
+plt.plot(surface_vel_z,'.-')
+# plot the acceleration of the surface
+acc = (surface_vel_z[1:] - surface_vel_z[:-1])/h_stepsize
+plt.plot(acc,'.-' )
+plt.legend(['z','z_vel','z_acc'])
+
+# plot force properties
+
---
+++ examples/basic_examples/unsteady_prescribed_wake/visualization.py
@@ -0,0 +1,184 @@
+import numpy as np
+
+'''
+list of variables to visualize are meshes and cell_data.set_vectors:
+wakes
+bound meshes
+forces
+kinematic velocities in body fixed frame ?
+accelerations in body fixed frame ?
+induced vel
+gamma? 
+'''
+
+import os
+
+def run_visualization(sim, h_stepsize,folder_name='',filename=''):
+    surface_names = ['wing']
+    num_nodes = sim[surface_names[0]].shape[0]
+    nx = sim[surface_names[0]].shape[1]
+    ny = sim[surface_names[0]].shape[2]
+    surface_shapes = [(num_nodes, nx, ny, 3)]
+    # 'bd_vtx_coords', 
+    # 'wake_coords',
+    visualize_mesh_names = ['bd_vtx_coords', 'wake_coords']
+    visualize_var_names = ['kinematic_vel']
+
+    import pyvista as pv
+
+    plotter = pv.Plotter()
+
+    panel_forces = sim['panel_forces_all'].copy()
+    coll_vel = sim['wing_coll_vel'].copy()
+
+    nframe = num_nodes
+    for i in range(len(surface_names)):
+        surface_name = surface_names[i]
+        mesh = sim[surface_name]
+        mesh_grid = pv.StructuredGrid(mesh[-1,:,:,0], mesh[-1,:,:,1], mesh[-1,:,:,2])
+        bound_mesh = sim[surface_name+'_bd_vtx_coords']
+        bound_grid = pv.StructuredGrid(bound_mesh[-1,:,:,0], bound_mesh[-1,:,:,1], bound_mesh[-1,:,:,2])
+        wake_mesh = sim['op_'+surface_name+'_wake_coords']
+        wake_grid = pv.StructuredGrid(wake_mesh[-1,:,:,0], wake_mesh[-1,:,:,1], wake_mesh[-1,:,:,2])
+        plotter.add_mesh(
+            mesh_grid,
+            # scalars=z.ravel(),
+            lighting=False,
+            show_edges=True,
+            scalar_bar_args={"title": "Height"},
+            clim=[-1, 1],
+        )
+        plotter.add_mesh(
+            bound_grid,
+            # scalars=z.ravel(),
+            lighting=False,
+            show_edges=True,
+            scalar_bar_args={"title": "Height"},
+            clim=[-1, 1],
+        )
+        plotter.add_mesh(
+            wake_grid,
+            # scalars=z.ravel(),
+            lighting=False,
+            show_edges=True,
+            scalar_bar_args={"title": "Height"},
+            clim=[-1, 1],
+        )
+        for phase in np.linspace(0, 2 * np.pi, nframe + 1)[:nframe]:
+            x_mesh = mesh[i,:,:,0]
+            y_mesh = mesh[i,:,:,1]
+            z_mesh = mesh[i,:,:,2]
+            x_m = bound_mesh[i,:,:,0]
+            y_m = bound_mesh[i,:,:,1]
+            z_m = bound_mesh[i,:,:,2]
+            x = wake_mesh[i,:,:,0]
+            y = wake_mesh[i,:,:,1]
+            z = wake_mesh[i,:,:,2]
+            grid_mesh = pv.StructuredGrid(x_mesh,y_mesh,z_mesh)
+            grid_bdmesh = pv.StructuredGrid(x_m,y_m,z_m)
+            grid = pv.StructuredGrid(x,y,z)
+            for vars in visualize_var_names:
+                var = sim[surface_name+'_'+vars]
+                var[:,:,0] = 0
+            grid_bdmesh.cell_data.set_vectors(np.swapaxes(panel_forces[i].reshape(nx-1,ny-1,3), 0,1).reshape(-1,3),'panel_forces')
+            grid_bdmesh.cell_data.set_vectors(np.swapaxes(coll_vel[i].reshape(nx-1,ny-1,3), 0,1).reshape(-1,3),'coll_vel')
+            isExist = os.path.exists(folder_name)
+            if not isExist:
+                os.makedirs(folder_name)
+                print('create folder',folder_name)
+            grid_mesh.save(folder_name+'/mesh'+filename+str(i)+'.vtk')
+            grid_bdmesh.save(folder_name+'/bd'+filename+str(i)+'.vtk')
+            grid.save(folder_name+'/wake'+filename+str(i)+'.vtk')
+            i+=1
+
+
+def run_visualization_eel(sim, h_stepsize):
+    surface_names = ['eel']
+    num_nodes = sim[surface_names[0]].shape[0]
+    nx = sim[surface_names[0]].shape[1]
+    ny = sim[surface_names[0]].shape[2]
+    surface_shapes = [(num_nodes, nx, ny, 3)]
+
+    # 'bd_vtx_coords', 
+    # 'wake_coords',
+    visualize_mesh_names = ['bd_vtx_coords', 'wake_coords']
+    visualize_var_names = ['kinematic_vel']
+
+    import pyvista as pv
+
+    plotter = pv.Plotter()
+
+    vel = sim['eel_kinematic_vel'].copy()
+    panel_forces_all = sim['panel_forces_all'].copy()
+    panel_forces_dynamic = sim['panel_forces_dynamic'].copy()
+    panel_forces = sim['panel_forces'].copy()
+
+    acc = np.zeros(vel.shape)
+    # we added a nagetive sign here to calculate the acceleration of the fish body
+    acc[:-1,:,:] = -(vel[1:,:,:] - vel[:-1,:,:])/h_stepsize
+    acc[-1,:,:] = -(vel[-1,:,:] - vel[-2,:,:])/h_stepsize
+
+    # forces = sim['eel_forces'].copy()
+    vel[:,:,0] = 0
+    nframe = num_nodes
+    for i in range(len(surface_names)):
+        surface_name = surface_names[i]
+        mesh = sim[surface_name]
+        mesh_grid = pv.StructuredGrid(mesh[-1,:,:,0], mesh[-1,:,:,1], mesh[-1,:,:,2])
+        bound_mesh = sim[surface_name+'_bd_vtx_coords']
+        bound_grid = pv.StructuredGrid(bound_mesh[-1,:,:,0], bound_mesh[-1,:,:,1], bound_mesh[-1,:,:,2])
+        wake_mesh = sim['op_'+surface_name+'_wake_coords']
+        wake_grid = pv.StructuredGrid(wake_mesh[-1,:,:,0], wake_mesh[-1,:,:,1], wake_mesh[-1,:,:,2])
+        plotter.add_mesh(
+            mesh_grid,
+            # scalars=z.ravel(),
+            lighting=False,
+            show_edges=True,
+            scalar_bar_args={"title": "Height"},
+            clim=[-1, 1],
+        )
+        plotter.add_mesh(
+            bound_grid,
+            # scalars=z.ravel(),
+            lighting=False,
+            show_edges=True,
+            scalar_bar_args={"title": "Height"},
+            clim=[-1, 1],
+        )
+        plotter.add_mesh(
+            wake_grid,
+            # scalars=z.ravel(),
+            lighting=False,
+            show_edges=True,
+            scalar_bar_args={"title": "Height"},
+            clim=[-1, 1],
+        )
+        for phase in np.linspace(0, 2 * np.pi, nframe + 1)[:nframe]:
+            x_mesh = mesh[i,:,:,0]
+            y_mesh = mesh[i,:,:,1]
+            z_mesh = mesh[i,:,:,2]
+            x_m = bound_mesh[i,:,:,0]
+            y_m = bound_mesh[i,:,:,1]
+            z_m = bound_mesh[i,:,:,2]
+            x = wake_mesh[i,:,:,0]
+            y = wake_mesh[i,:,:,1]
+            z = wake_mesh[i,:,:,2]
+            grid_mesh = pv.StructuredGrid(x_mesh,y_mesh,z_mesh)
+            grid_bdmesh = pv.StructuredGrid(x_m,y_m,z_m)
+            grid = pv.StructuredGrid(x,y,z)
+            for vars in visualize_var_names:
+                var = sim[surface_name+'_'+vars]
+                var[:,:,0] = 0
+            grid_bdmesh.cell_data.set_vectors(np.swapaxes(vel[i].reshape(nx-1,ny-1,3), 0,1).reshape(-1,3),vars)
+            grid_bdmesh.cell_data.set_vectors(np.swapaxes(panel_forces_all[i].reshape(nx-1,ny-1,3), 0,1).reshape(-1,3),'panel_forces_all')
+            grid_bdmesh.cell_data.set_vectors(np.swapaxes(panel_forces_dynamic[i].reshape(nx-1,ny-1,3), 0,1).reshape(-1,3),'panel_forces_dynamic')
+            grid_bdmesh.cell_data.set_vectors(np.swapaxes(panel_forces[i].reshape(nx-1,ny-1,3), 0,1).reshape(-1,3),'panel_forces')
+            grid_bdmesh.cell_data.set_vectors(np.swapaxes(acc[i].reshape(nx-1,ny-1,3), 0,1).reshape(-1,3),'acc')
+            # print(i,grid.points.shape)
+            grid_mesh.save('eel_vtks/mesh'+str(i)+'.vtk')
+            grid_bdmesh.save('eel_vtks/bd'+str(i)+'.vtk')
+            grid.save('eel_vtks/wake'+str(i)+'.vtk')
+            i+=1
+
+
+
